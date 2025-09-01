5611008112246cefe312a7ca14f2a86e49b53eb2
lanassilva0 <lanassilvalss@gmail.com>
2025-09-01 02:44:56 -0300

    fix: ajustes das rotas

--- hub-sw_front/src/router/index.js
+++ hub-sw_front/src/router/index.js
@@ -1,46 +1,28 @@
-import { createRouter, createWebHistory } from "vue-router";
+import Vue from "vue";
+import Router from "vue-router";
 
 const LoginPage = () => import("@/views/loginPage.vue");
 const HomePage = () => import("@/views/homePage.vue");
 
-const routes = [
-  {
-    path: "/login",
-    name: "Login",
-    component: LoginPage,
-    meta: {
-      allowAnonymous: true,
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/login",
+      name: "Login",
+      component: LoginPage,
+      meta: {
+        allowAnonymous: true,
+      },
     },
-  },
-  {
-    path: "/home",
-    name: "Home",
-    component: HomePage,
-    meta: {
-      breadcrumb: [{ name: "dashboard" }],
+    {
+      path: "/home",
+      name: "Home",
+      component: HomePage,
+      meta: {
+        breadcrumb: [{ name: "dashboard" }],
+      },
     },
-  },
-];
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
+  ],
 });
-
-router.beforeEach((to, from, next) => {
-  const isAuthenticated = !!localStorage.getItem("token");
-  const requiresAuth = to.meta.requiresAuth;
-
-  if (requiresAuth && !isAuthenticated) {
-    next("/login");
-  } else if (to.name === "Login" && isAuthenticated) {
-    next("/home");
-  } else {
-    document.title = to.meta.title
-      ? `${to.meta.title} - Minha App`
-      : "Minha App";
-    next();
-  }
-});
-
-export default router;
