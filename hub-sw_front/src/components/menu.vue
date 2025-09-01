583713235f3f9c295c03bb5b40592df2923d8726
lanassilva0 <lanassilvalss@gmail.com>
2025-09-01 02:46:00 -0300

    feat: implementação inicial do componente do menu

---
+++ hub-sw_front/src/components/menu.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <v-navigation-drawer app v-if="isAuthenticated" v-model="drawer">
+      <v-list dense nav>
+        <v-list-item link to="/home">
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<style scoped>
+
+</style>
